1e87f8e782c99241c76ea9e5e83ae46b9f1774e5
EleoRomario <eleo.romario@gmail.com>
2022-04-29 15:28:19 -0500

    init and activity 01

---
+++ app/src/main/java/com/idnp/danp_lab01_animacinconmotionlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.idnp.danp_lab01_animacinconmotionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad1_01);
+    }
+}
