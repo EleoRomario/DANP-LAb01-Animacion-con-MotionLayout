d299d423eabdbe129011977ba502100769d76ae2
EleoRomario <eleo.romario@gmail.com>
2022-04-29 20:08:01 -0500

    mejorado la actividad 1 y organizar ambas actividades

--- app/src/main/java/com/idnp/danp_lab01_animacinconmotionlayout/MainActivity.java
+++ app/src/main/java/com/idnp/danp_lab01_animacinconmotionlayout/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actividad1_01);
+        setContentView(R.layout.main_activity);
     }
 }
