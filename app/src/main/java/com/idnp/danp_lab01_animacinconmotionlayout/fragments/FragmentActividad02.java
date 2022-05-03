cbf8804e1f58153ca99736ea76a7e5f7622781aa
EleoRomario <eleo.romario@gmail.com>
2022-05-02 22:08:13 -0500

    terminate actividad

--- app/src/main/java/com/idnp/danp_lab01_animacinconmotionlayout/fragments/FragmentActividad02.java
+++ app/src/main/java/com/idnp/danp_lab01_animacinconmotionlayout/fragments/FragmentActividad02.java
@@ -2,11 +2,15 @@ package com.idnp.danp_lab01_animacinconmotionlayout.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.idnp.danp_lab01_animacinconmotionlayout.R;
 
